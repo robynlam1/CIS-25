d05c199a242c9edb6537bd84584cd5d961fc8f9d
robynlam1 <106898486+robynlam1@users.noreply.github.com>
2026-06-22 13:59:00 -0700

    Update inventory.cpp

--- Module 2/inventory.cpp
+++ Module 2/inventory.cpp
@@ -1,1 +1,9 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello Inventory System!\n"
+  cout << "\tSize of int: " << sizeof(int) << "bytes\n";
+  cout << "\tSize of float: " << sizeof(float) << "bytes\n";
+  cout << "\tSize of char: " << sizeof(char) << "bytes\n";
+  return 0;
+}
