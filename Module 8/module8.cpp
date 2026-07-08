b3836b943d8776868ecc0888f55af5b577cdb568
robynlam1 <106898486+robynlam1@users.noreply.github.com>
2026-07-08 01:09:44 -0700

    Update module8.cpp

--- Module 8/module8.cpp
+++ Module 8/module8.cpp
@@ -181,7 +181,7 @@ void saveToFile(Patient* patients, int count) {
 void sortPatients(Patient* patients, int count) {
     for (int i = 0; i < count - 1; i++) {
         for (int j = 0; j < count - i - 1; j++) {
-            if (patients[j].id > patients[j + 1].ID) {
+            if (patients[j].ID > patients[j + 1].ID) {
                 Patient temp = patients[j];
                 patients[j] = patients[j + 1];
                 patients[j + 1] = temp;
